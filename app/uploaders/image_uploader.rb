b9a626e9fb38538f0395c09e0d25424e69fbf33d
Ubuntu <vagrant@Base-Box>
2017-04-15 02:35:50 +0000

    fix the image tag with the link_to.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,6 +34,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [150, 225]
   end
 
+  version :small do
+    process resize_to_fit: [50, 120]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
