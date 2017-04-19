f7808213a3b26a639f41f229cf808e1e9b10fc38
Ubuntu <george880131@gmail.com>
2017-04-19 23:35:27 +0000

    change model for heroku

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,6 +14,6 @@ class Book < ApplicationRecord
 
 
   scope :search_by_name, lambda {|query|where(['"books"."name" LIKE ?', "%#{query}%"])}
-  scope :search_by_type, lambda {|query|where(['"books"."type" LIKE ?', "%#{query}%"])}
-  scope :search_by_status, lambda {|query|where(['"books"."status" LIKE ?', "%#{query}%"])}
+  scope :search_by_type, lambda {|query|where(['type LIKE ?', "%#{query}%"])}
+  scope :search_by_status, lambda {|query|where(['status LIKE ?', "%#{query}%"])}
 end
